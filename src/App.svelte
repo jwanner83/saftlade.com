5aee0cbbeae0ebe7e120c334d77ff4eb941d6fa5
Jonas Wanner <jonas@wanner.com>
2021-09-21 19:48:50 +0200

    feat: implement components into App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,38 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import Navigation from './lib/Navigation/Navigation.svelte'
+  import Section from './lib/Layout/Section.svelte'
+  import Offer from './lib/Pages/Offer/Offer.svelte'
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
+  <Navigation />
 
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <Section title="Angebot">
+    <Offer />
+  </Section>
+  <Section title="Aktion">
+    <p>deger</p>
+  </Section>
 </main>
 
 <style>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    --primary-light: #48A8BC29;
+    --primary: #48A8BC;
+
+    --dark-gray: #636363;
+
+    --primary-background: #000;
+    --secondary-background: #2B2B2B;
+
+    --box-radius: 10px;
+    --box-radius-small: 5px;
   }
 
   main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+    overflow: hidden;
+    min-height: 100%;
+    width: 100%;
+    background: var(--primary-background);
   }
 </style>

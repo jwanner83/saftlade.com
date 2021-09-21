080019dff3cd55ea6a6107783b2a514e3895eb8f
Jonas Wanner <jonas@wanner.com>
2021-09-21 21:10:09 +0200

    feat: adding intersection observer to change navigation on intersect

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
+    import { onMount } from 'svelte'
     import NavigationItem from './NavigationItem.svelte'
 
     let active: number = 1
+    const observer = new IntersectionObserver(onEntry, {
+        threshold: [0.5]
+    })
 
     function setActive(index: number): void {
         active = index
     }
 
+    function onEntry(entry) {
+        setActive(entry[0].target.id === 'angebot' ? 1 : 2)
+    }
+
+    onMount(async () => {
+        document.querySelectorAll('[data-section]').forEach(element => {
+            observer.observe(element)
+        })
+    })
 </script>
 
 <div class="navigation__wrapper">
@@ -32,7 +45,7 @@
     padding: 10px;
     display: flex;
     gap: 10px;
-    border-radius: var(--box-radius);
+    border-radius: var(--border-radius);
 
     &__wrapper {
       z-index: 10;

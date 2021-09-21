fcd1c8cb89b8ae4aaa89a3fbbc1b2e919a5736dd
Jonas Wanner <jonas@wanner.com>
2021-09-21 19:48:27 +0200

    feat: adding navigation

---
+++ src/lib/Navigation/Navigation.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import NavigationItem from './NavigationItem.svelte'
+
+    let active: number = 1
+
+    function setActive(index: number): void {
+        active = index
+    }
+
+</script>
+
+<div class="navigation__wrapper">
+    <div class="navigation">
+        <NavigationItem title="Angebot"
+                        type="{active === 1 ? 'active' : 'inactive'}"
+                        link="angebot"
+                        on:click={() => setActive(1)}
+        />
+        <NavigationItem title="Aktion"
+                        type="{active === 2 ? 'active' : 'inactive'}"
+                        link="aktion"
+                        on:click={() => setActive(2)}
+        />
+
+        <NavigationItem title="Kontakt" type="dominant"/>
+    </div>
+</div>
+
+<style lang="scss">
+  .navigation {
+    background: var(--secondary-background);
+    padding: 10px;
+    display: flex;
+    gap: 10px;
+    border-radius: var(--box-radius);
+
+    &__wrapper {
+      z-index: 10;
+      position: fixed;
+      top: 0;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      padding: 40px 0;
+    }
+  }
+</style>
---
+++ src/lib/Navigation/NavigationItem.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    export let title: string
+    export let type: string
+    export let link: string
+</script>
+
+{#if link}
+    <a on:click class="navigation__item navigation__item--{type}" href="#{link}">
+        {title}
+    </a>
+{:else}
+    <button on:click class="navigation__item navigation__item--{type}">
+        {title}
+    </button>
+{/if}
+
+<style lang="scss">
+  .navigation__item {
+    text-decoration: none;
+    cursor: pointer;
+    transition: 0.3s;
+    padding: 16px 12px 12px;
+    text-align: center;
+    width: 200px;
+    font-weight: bold;
+    font-size: 18px;
+    border: none;
+    border-radius: var(--box-radius-small);
+
+    &--active {
+      background: var(--primary-light);
+      color: var(--primary)
+    }
+
+    &--inactive {
+      background: none;
+      color: var(--dark-gray)
+    }
+
+    &--dominant {
+      background: white;
+      color: black;
+    }
+
+    &:hover {
+      opacity: 0.7;
+    }
+
+    &:active {
+      transform: scale(0.95);
+    }
+  }
+</style>

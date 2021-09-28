8f12061db67a82d35fcf112673f86cd29275c477
jwa <jonas.wanner@clavisit.com>
2021-09-28 18:01:47 +0200

    feat: rebrand logo

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -4,7 +4,7 @@
 
     let active: number = 1
     const observer: IntersectionObserver = new IntersectionObserver(onEntry, {
-        threshold: 0.7
+        threshold: 0.5
     })
 
     function setActive(index: number): void {
@@ -23,6 +23,7 @@
 </script>
 
 <div class="navigation__wrapper">
+    <img class="navigation__favicon" src="favicon.png" alt="favicon">
     <div class="navigation">
         <NavigationItem title="Angebot"
                         type="{active === 1 ? 'active' : 'inactive'}"
@@ -34,47 +35,48 @@
                         link="#aktion"
                         on:click={() => setActive(2)}
         />
-
-        <NavigationItem title="Kontakt" type="dominant" link="mailto:jonas@wanner.com?subject=Saftlade: Kontaktanfrage"/>
     </div>
 </div>
 
 <style lang="scss">
   .navigation {
-    background: var(--secondary-background);
-    padding: 10px;
     display: flex;
-    gap: 10px;
-    border-radius: var(--border-radius);
+    align-items: center;
     animation: transition-in 0.6s;
     animation-delay: 1s;
     animation-fill-mode: both;
 
-    @media (max-width: 850px) {
-      padding: 5px;
-      gap: 5px;
+    &__favicon {
+      height: 100%;
+      width: 50px;
+      animation: transition-in 0.6s;
+      animation-delay: 0.8s;
+      animation-fill-mode: both;
     }
 
     &__wrapper {
       overflow: hidden;
       z-index: 10;
       position: sticky;
-      top: 0;
+      top: 40px;
       width: 100%;
       display: flex;
-      justify-content: center;
-      padding: 40px 0;
+      justify-content: space-between;
+      padding: 0 80px;
+      max-width: 800px;
+      margin: 80px auto;
+      text-align: center;
     }
   }
 
   @keyframes transition-in {
     from {
       opacity: 0;
-      transform: translateY(100%) scale(0.7);
+      transform: translateY(-200%);
     }
     to {
       opacity: 1;
-      transform: translateY(0) scale(1);
+      transform: translateY(0);
     }
   }
 </style>

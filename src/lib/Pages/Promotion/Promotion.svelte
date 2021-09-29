cbe47e3bc9aedff939b8c12ebdbaf270417b3904
jwa <jonas.wanner@clavisit.com>
2021-09-29 16:52:52 +0200

    feat: including second promotion

--- src/lib/Pages/Promotion/Promotion.svelte
+++ src/lib/Pages/Promotion/Promotion.svelte
@@ -1,21 +1,28 @@
 <div class="promotion">
-    <div class="promotion__main">
-        <div class="promotion__main-percent">%</div>
-        <h1 class="promotion__main-title">Pfirsich Eistee Ausverkauf</h1>
+    <div class="promotion__main promotion__main--redbull">
+        <div class="promotion__main-background">NEU</div>
+        <h1 class="promotion__main-subtitle">NEU VERFÜGBAR</h1>
+        <h1 class="promotion__main-title">Redbull</h1>
         <p class="promotion__main-text">
-            Profitieren sie jetzt von<br>unfassbaren 5%* auf den<br>Pfirsich Eistee!
+            Profitieren sie jetzt von einem unschlagbaren Einführungspreis.*
+        </p>
+    </div>
+    <div class="promotion__main promotion__main--icetea">
+        <div class="promotion__main-background">%</div>
+        <h1 class="promotion__main-title">Eistee<br> Ausverkauf</h1>
+        <p class="promotion__main-text">
+            Alles muss raus! Jetzt von legendären 10%** auf den Eistee profitieren!
         </p>
     </div>
     <div class="promotion__wrapper">
         <div class="promotion__information">
             <p class="promotion__information-text">
-                * 4,7619% auf den Preis von einem Pfirsich Eistee. Angebot gilt bis am 10. Oktober und kann jederzeit
-                unbegründet zurückgezogen werden. Der QR Code für das Angebot kann bei der Verkaufsstelle gefunden
-                werden.
+                * Der Einführungspreis von 1.50 CHF pro Dose ist bis am 10. Oktober 2021 gültig.
             </p>
-        </div>
-        <div class="promotion__call">
-            <p class="promotion__call-text">Jetzt sparen!</p>
+            <p class="promotion__information-text">
+                ** 9,52381% auf den Preis von einem Eistee. Angebot ist bis am 12. Oktober 2021 gültig oder solange
+                vorrat.
+            </p>
         </div>
     </div>
 </div>
@@ -29,23 +36,31 @@
     &__main {
       overflow: hidden;
       position: relative;
-      padding: 40px;
+      padding: 80px 40px 40px;
       background: linear-gradient(128deg, #3388ff 0%, hsl(0, 75%, 50%) 100%);
       color: #fff;
       text-align: left;
       border-radius: var(--border-radius);
 
-      &-percent {
+      &-background {
         position: absolute;
         right: 0;
         margin: -200px;
         font-size: 900px;
         font-weight: bold;
-        opacity: 0.5;
+        opacity: 0.3;
+      }
+
+      &-subtitle {
+        margin-bottom: 8px;
+        margin-left: 4px;
+        font-weight: lighter;
+        font-size: 26px;
       }
 
       &-title {
         font-size: 80px;
+        margin-top: 0;
         margin-bottom: 20px;
       }
 
@@ -55,12 +70,26 @@
         margin-bottom: 0;
         line-height: 50px;
       }
+
+      &--redbull {
+        background: linear-gradient(115deg, #452730 0%, hsl(250, 76%, 50%) 100%);
+
+        .promotion__main-background {
+          top: 0;
+          left: 0;
+          margin: -8px 0 0 -260px;
+          font-size: 560px;
+        }
+      }
+
+      &--icetea {
+        background: linear-gradient(230deg, #1bc0ad 0%, hsl(259, 76%, 50%) 100%);
+      }
     }
 
     &__wrapper {
       display: grid;
       grid-gap: 40px;
-      grid-template-columns: 3fr 1fr;
     }
 
     &__information {
